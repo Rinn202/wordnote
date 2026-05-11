f026582afef801b2f47d6e7300e429fc802fd130
Rinn202 <dioneo54@gmail.com>
2026-05-11 15:54:12 +0900

    테스크 더미데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-SELECT 1;
+-- 초기 Task 데이터
+INSERT INTO task (task_id, name) VALUES (1, '투약');
+INSERT INTO task (task_id, name) VALUES (2, 'v/s측정');
+INSERT INTO task (task_id, name) VALUES (3, '드레싱');
+INSERT INTO task (task_id, name) VALUES (4, '루틴차팅');
