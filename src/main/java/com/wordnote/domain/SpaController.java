856c461332f81ac0454da348b91730f09e76b726
Rinn202 <dioneo54@gmail.com>
2026-05-23 18:33:26 +0900

    서버 배포용 설정추가

---
+++ src/main/java/com/wordnote/domain/SpaController.java
@@ -0,0 +1,12 @@
+package com.wordnote.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+    @RequestMapping(value = {"/", "/{path:[^\\.]*}"})
+    public String index() {
+        return "forward:/index.html";
+    }
+}
